d726ba0a06475fdd6415026c9fdd51496ed43436
kyh174 <kyh174@naver.com>
2022-07-17 13:07:25 +0900

    0717_링크연결
    - Link 컴포넌트 사용
    - to="경로" 로 URL 지정

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+import './App.css';
+
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper!</h1>
+      <h1>Bookkeeper</h1>
+      <nav className='nav-bar'>
+        <Link to='/invoices'>Invoices</Link> |{" "}
+        <Link to='/expenses'>Expenses</Link>
+      </nav>
     </div>
   );
 }
