7d0b7eeef7eb652652c535c515c187c79d2ea371
kyh174 <kyh174@naver.com>
2022-07-17 14:18:17 +0900

    0717_데이터 가져오기

--- router-tutorial/src/routes/invoices.jsx
+++ router-tutorial/src/routes/invoices.jsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+import { getInvoices } from '../data';
+
 export default function Invoices() {
+  let invoices = getInvoices();
   return (
-    <main className="invoices-main">
-      <h2>Invoices</h2>
-    </main>
+    <div className='invoices-div'>
+      <nav className='invoices-nav'>
+        {invoices.map((invoice) => (
+          <Link
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }
