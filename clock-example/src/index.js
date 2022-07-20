88d247b8c584cb4d4cfab46c8111c1ee5859bfee
kyh174 <kyh174@naver.com>
2022-07-20 11:26:22 +0900

    0720_Clock 예제 성공

--- clock-example/src/index.js
+++ clock-example/src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { useState } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Clock(props) {
+function Clock() {
+  const [date, setDate] = useState(new Date());
+  const updateTime = () => setDate(new Date());
+  
+  setInterval(updateTime, 1000);
+
   return (
     <div>
       <h1>Hello, world</h1>
-      <h2>It is {props.date.toLocaleTimeString()}</h2>
+      <h2>It is {date.toLocaleTimeString()}</h2>
     </div>
   );
 }
 
-function tick() {
-  root.render(<Clock date={new Date()} />);
-}
-
-setInterval(tick, 1000);
+root.render(<Clock />);
